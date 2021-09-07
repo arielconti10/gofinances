7027ca04e37623d5d971f0c8485d185d3f69a49c
Ariel Conti <arielconti10@gmail.com>
2021-09-07 15:00:29 -0300

    feat: adding icon

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,15 +1,40 @@
 import React from "react";
-import { Image } from "react-native";
 
-import { Container, Header} from "./styles";
+import {
+  Container,
+  Header,
+  UserInfo,
+  Avatar,
+  User,
+  UserGreeting,
+  UserName,
+  UserWrapper,
+  Icon,
+} from "./styles";
 
 import Logo from "../../assets/images/skteworld.png";
+import { RFValue } from "react-native-responsive-fontsize";
 
 export function Dashboard() {
   return (
     <Container>
-      <Header></Header>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Avatar
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/17475188?v=4",
+              }}
+            />
 
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Ariel</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components/native";
-import { RFPercentage } from "react-native-responsive-fontsize";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import { Feather } from "@expo/vector-icons";
 
 export const Container = styled.View`
   flex: 1;
@@ -9,7 +10,53 @@ export const Container = styled.View`
 export const Header = styled.View`
   width: 100%;
   height: ${RFPercentage(42)}px;
-
-  
   background-color: ${({ theme }) => theme.colors.primary};
+
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const UserWrapper = styled.View`
+  width: 100%;
+  padding: 0 24px;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const UserInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Avatar = styled.Image`
+  width: ${RFValue(48)}px;
+  height: ${RFValue(48)}px;
+
+  border-radius: 10px;
+`;
+
+export const User = styled.View`
+  margin-left: 17px;
+`;
+
+export const UserGreeting = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(18)}px;
+`;
+
+export const UserName = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+
+  font-family: ${({ theme }) => theme.fonts.bold};
+  font-size: ${RFValue(18)}px;
+`;
+
+export const Icon = styled(Feather)`
+  color: ${({ theme }) => theme.colors.secondary};
+  font-size: ${RFValue(24)}px;
 `;
