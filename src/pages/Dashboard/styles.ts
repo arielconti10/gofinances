3fee8da86f3464aad5b2a668d69ae0c781800d45
Ariel Conti <arielconti10@gmail.com>
2021-09-07 21:50:36 -0300

    feat: added TransactionCard component

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -72,3 +72,15 @@ export const HighLightCards = styled.ScrollView.attrs({
   position: absolute;
   margin-top: ${RFPercentage(20)}px;
 `;
+
+export const Transactions = styled.View`
+  flex: 1;
+  padding: 0 24px;
+  margin-top: ${RFPercentage(12)}px;
+`;
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(18)}px;
+
+  margin-bottom: 16px;
+`;
