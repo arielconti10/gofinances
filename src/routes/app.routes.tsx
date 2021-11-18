9c628429a8daf3cb57db40d5c3c25665bd9a815f
Ariel Conti <arielconti10@gmail.com>
2021-11-18 09:32:07 -0300

    feat: add icons to bottom tab

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,24 +1,68 @@
 import React from 'react';
+import { Platform } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons'
+import { useTheme } from 'styled-components';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 import { Dashboard } from '../pages/Dashboard';
 import { Register } from '../pages/Register';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function AppRoutes() {
+  const theme = useTheme();
+
   return (
-    <Navigator>
+    <Navigator 
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: theme.colors.secondary,
+        tabBarInactiveTintColor: theme.colors.text,
+        tabBarLabelPosition: 'beside-icon',
+        tabBarStyle: {
+          height: 88,
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+        }
+      }}
+    >
       <Screen 
         name="Listagem" 
         component={Dashboard} 
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons 
+              name="format-list-bulleted"
+              size={size}
+              color={color}
+            />
+          )
+        }}
       />
       <Screen 
         name="Cadastrar" 
         component={Register} 
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons 
+              name="attach-money"
+              size={size}
+              color={color}
+            />
+          )
+        }}
       />
       <Screen 
         name="Resumo" 
         component={Dashboard} 
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons 
+              name="pie-chart"
+              size={size}
+              color={color}
+            />
+          )
+        }}
       />
 
     </Navigator>
